cc26601a1c6cd56345cc29beef2c2bfac5d050e4
Felipe Jun Nishitani <felipejunnishitani>
2025-09-10 20:40:30 -0300

    Inicializacao

--- AES.py
+++ AES.py
@@ -1,5 +1,31 @@
+import numpy as np
+
 def criptografar(texto, chave):
-    pass
+    # Inicialização
+    texto_bytes = texto.encode('utf-8') # transformação da string em bytes
+    texto_bytes = texto_bytes.ljust(16, b'\x00') # preenche com byte zero
+
+    estado = np.zeros((4,4), dtype=np.uint8) # inicialização matriz 4x4 de bytes
+
+    k = 0
+    for i in range(4):
+        for j in range(4):
+            estado[j, i] = texto_bytes[k] # preenche matriz por coluna
+            k += 1
+
+
+    # Rodada inicial -> operação xor
+
+    # Rodadas intermediárias
+        # Substituição dos bytes
+
+        # Deslocamento das linhas
+
+        # Mix de colunas
+        
+        # Adicionar roundkey
+
+    # Rodada final
 
 def descriptografar(texto, chave):
     pass
